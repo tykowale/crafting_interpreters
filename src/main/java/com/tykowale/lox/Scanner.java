f6addc622fc8d204e0788fad5e4d571971af0ce9
tykowale <tykowale@gmail.com>
2020-03-20 17:41:44 -0700

    add string parsing

--- src/main/java/com/tykowale/lox/Scanner.java
+++ src/main/java/com/tykowale/lox/Scanner.java
@@ -91,10 +91,35 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"':
+                string();
+                break;
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+            }
+            advance();
+        }
+
+        // Unterminated string
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing "
+        advance();
+
+        // Trim the surrounding quotes
+        String value = source.substring(start + 1, current -1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
@@ -102,7 +127,7 @@ public class Scanner {
             return false;
         }
 
-        if (source.charAt(current) != expected) {
+        if (peek() != expected) {
             return false;
         }
 
