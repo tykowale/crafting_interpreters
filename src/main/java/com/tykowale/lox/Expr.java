7f0fe2192c943d9a47057f3bee04a56da4400765
tykowale <tykowale@gmail.com>
2020-03-21 17:35:58 -0700

    generate ast and be able to print it out

--- src/main/java/com/tykowale/lox/Expr.java
+++ src/main/java/com/tykowale/lox/Expr.java
@@ -1,41 +1,68 @@
 package com.tykowale.lox;
 
+import java.util.List;
+
 public abstract class Expr {
-    public static class Binary extends Expr {
-        Binary(Expr left, Token operator, Expr right) {
-            this.left = left;
-            this.operator = operator;
-            this.right = right;
-        }
-
-        final Expr left;
-        final Token operator;
-        final Expr right;
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
+  public static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
     }
 
-    public static class Grouping extends Expr {
-        Grouping(Expr expression) {
-            this.expression = expression;
-        }
-
-        final Expr expression;
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
     }
 
-    public static class Literal extends Expr {
-        Literal(Object value) {
-            this.value = value;
-        }
-
-        final Object value;
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+  public static class Grouping extends Expr {
+    Grouping(Expr expression) {
+      this.expression = expression;
     }
 
-    public static class Unary extends Expr {
-        Unary(Token operator, Expr right) {
-            this.operator = operator;
-            this.right = right;
-        }
-
-        final Token operator;
-        final Expr right;
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
     }
+
+    final Expr expression;
+  }
+  public static class Literal extends Expr {
+    Literal(Object value) {
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+
+    final Object value;
+  }
+  public static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
+    final Token operator;
+    final Expr right;
+  }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
--- src/main/java/com/tykowale/tool/GenerateAst.java
+++ src/main/java/com/tykowale/tool/GenerateAst.java
@@ -32,11 +32,18 @@ public class GenerateAst {
         writer.println();
         writer.println("public abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
+        // The AST classes
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        // The base accept() method
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
@@ -49,7 +56,8 @@ public class GenerateAst {
         writer.println("    " + className + "(" + fieldList + ") {");
 
         // Store parameters in fields
-        String[] fields = fieldList.split(", ");;
+        String[] fields = fieldList.split(", ");
+        ;
         for (String field : fields) {
             String name = field.split(" ")[1];
             writer.println("      this." + name + " = " + name + ";");
@@ -57,10 +65,28 @@ public class GenerateAst {
 
         writer.println("    }");
 
+        // Visitor pattern
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" + className + baseName + "(this);");
+        writer.println("    }");
+
         // Fields
         writer.println();
         for (String field : fields) {
             writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
         }
 
         writer.println("  }");
